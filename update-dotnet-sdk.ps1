fa2fce2f9e0195e9cacc4a14f6d0785c06a5f6af
martincostello <martin@martincostello.com>
2020-04-18 16:09:11 +0100

    Add script shell
    Add the shell of the script to contain the logic.

---
+++ update-dotnet-sdk.ps1
@@ -0,0 +1,6 @@
+#! /usr/bin/pwsh
+param(
+    [Parameter(Mandatory = $false)][string] $Channel = "3.1",
+    [Parameter(Mandatory = $false)][string] $UserName = "",
+    [Parameter(Mandatory = $false)][string] $UserEmail = ""
+)
